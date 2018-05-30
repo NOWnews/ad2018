1951d7f33bd0c48ba9244b64e5691429450bea05
LiweiTW <Vincent.cdw@gmail.com>
2018-05-30 17:56:45 +0800

    Table migration.

---
+++ database/migrations/2018_05_30_045021_create_inventories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInventoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->enum('type', ['text', 'image']);
+            $table->string('name');
+            $table->string('desc');
+            $table->string('password');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventories');
+    }
+}
---
+++ database/migrations/2018_05_30_073551_create_creatives_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCreativesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('creatives', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('order_ic');
+            $table->enum('type', ['text', 'image']);
+            $table->string('title');
+            $table->string('image')->nullable();;
+            $table->string('link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('creatives');
+    }
+}
