901e7017a3b0e72579ad5248b0e03bee3248d049
LiweiTW <Vincent.cdw@gmail.com>
2018-06-06 11:06:16 +0800

    Add queue migration file.

---
+++ database/migrations/2018_06_06_021924_create_queue_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQueueTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('queue', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('creative_id');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('queue');
+    }
+}
