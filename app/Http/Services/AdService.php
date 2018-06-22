a0c6b651e59043325c3731900ad93e68586643f9
LiWei <Vincent.cdw@gmail.com>
2018-06-22 23:17:31 +0800

    Update AdService.php

--- app/Http/Services/AdService.php
+++ app/Http/Services/AdService.php
@@ -35,27 +35,30 @@ class AdService
 
         if (!$creatives) {
             $inventory = Inventory::where(['id' => $inventoryId])->get()->first();
-            $creatives = $inventory->creatives->filter(function ($value) {
-                $res = false;
-                if ($value->status) {
-                    foreach ($value->queues as $queue){
-                        $startTime = new Carbon($queue->start_time, "Asia/Taipei");
-                        $endTime = new Carbon($queue->end_time, "Asia/Taipei");
-                        $now = new Carbon();
-                        if ($startTime<=$now && $endTime>=$now){
-                            return true;
+            $creatives = $inventory->creatives;
+            if ($creatives) {
+                $creatives = $inventory->creatives->filter(function ($value) {
+                    $res = false;
+                    if ($value->status) {
+                        foreach ($value->queues as $queue){
+                            $startTime = new Carbon($queue->start_time, "Asia/Taipei");
+                            $endTime = new Carbon($queue->end_time, "Asia/Taipei");
+                            $now = new Carbon();
+                            if ($startTime<=$now && $endTime>=$now){
+                                return true;
+                            }
                         }
+
                     }
+                    return $res;
+                });
 
-                }
-                return $res;
-            });
+                $creatives = $creatives->map(function ($creative) {
+                    return $this->adFactory($creative);
+                });
 
-            $creatives = $creatives->map(function ($creative) {
-                return $this->adFactory($creative);
-            });
-
-            Cache::put('ads/' . $inventoryId, $creatives, env('AD_CACHE_TIME', 5));
+                Cache::put('ads/' . $inventoryId, $creatives, env('AD_CACHE_TIME', 5));
+            }
         }
 
 
