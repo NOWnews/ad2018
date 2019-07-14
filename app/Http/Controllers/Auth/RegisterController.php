fd04aa68b0949a93f4d300a8ece8c4dce2b9ce6d
Div <b510336@gmail.com>
2019-07-15 00:50:03 +0800

    restrict emails to view register page

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -37,13 +37,13 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest');
+        $this->middleware('auth');
     }
 
     /**
      * Get a validator for an incoming registration request.
      *
-     * @param  array  $data
+     * @param array $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
     protected function validator(array $data)
@@ -58,7 +58,7 @@ class RegisterController extends Controller
     /**
      * Create a new user instance after a valid registration.
      *
-     * @param  array  $data
+     * @param array $data
      * @return \AD2018\User
      */
     protected function create(array $data)
@@ -68,5 +68,19 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+    }
+
+    public function showRegistrationForm()
+    {
+        $userEmail = auth()->user()->email;
+        $allowEmails = [
+            'jiaing@nownews.com',
+        ];
+
+        if (!in_array($userEmail, $allowEmails)) {
+            return view('home');
+        }
+
+        return view('auth.register');
     }
 }
